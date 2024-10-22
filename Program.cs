5cbb2e945a6187ed4d378668fe0b1fa02ce9d9ed
MissKluck <sigrid.b.kluck@gmail.com>
2024-10-22 09:59:59 +0200

    initial setup + array setup

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace undervisning_22._10;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] positiveNumbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        string[] names = {"Lars", "Jakob", "Eirik", "BAX", "Trym Magnus", "Sjaman Durek", "Harald Hårfagre"};
+
+        // For = bruke for spesifikke, foreach = bruke for å få ut alle
+        for(int i = 0; i < 5; i++)
+        {
+            Console.WriteLine(positiveNumbers[i]);
+        }
+    }
+}
