efcc853f1e84153ba070406617e18965a474541b
MissKluck <sigrid.b.kluck@gmail.com>
2024-10-22 10:24:51 +0200

    added new names to array

--- Program.cs
+++ Program.cs
@@ -5,7 +5,6 @@ class Program
     static void Main(string[] args)
     {
         int[] positiveNumbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
         string[] names = {"Lars", "Jakob", "Eirik", "BAX", "Trym Magnus", "Sjaman Durek", "Harald Hårfagre"};
 
         // For = bruke for spesifikke, foreach = bruke for å få ut alle
@@ -15,10 +14,14 @@ class Program
         }
 
         var newNames = names.Append("Ida");
+        newNames = newNames.Append("Jon");
+
         foreach(string name in newNames)
         {
             Console.WriteLine(name);
         }
+
+        
 
     }
 }
